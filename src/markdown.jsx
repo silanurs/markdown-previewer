ec3b29dc0a0d345af02238a9a55f5903ac5f1a12
silanurs <sarii.silanur@gmail.com>
2023-10-21 15:36:15 +0300

    build : added react-redux library

--- src/markdown.jsx
+++ src/markdown.jsx
@@ -1,7 +1,7 @@
 export default function Preview() {
 return (
     <div className="preview">
-        <h3 className="titlePreview">Preview</h3>
+        <h3 className="titlePreview">Previewer</h3>
         <div id="preview"></div>
 
     </div>
